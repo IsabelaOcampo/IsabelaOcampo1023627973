2424eea94f5dc2d1ee848435a43f898cf0b12d76
IsabelaOcampo <isabelaocampo1124596@correo.itm.edu.co>
2026-05-31 03:48:24 -0500

    Update README.md

--- Program.cs
+++ Program.cs
@@ -1,1 +1,4 @@
 ﻿Console.WriteLine("Hola desde el CI/CD - Cambio 1");
+Console.WriteLine("Hola desde el CI/CD - Cambio 2");
+Console.WriteLine("Pipeline funcionando correctamente");
+Console.WriteLine("Isabela Ocampo - 1023627973");
